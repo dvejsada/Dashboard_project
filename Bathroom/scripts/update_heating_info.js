614d33167594ffa5aeb515d6dfceed17773dd5a7
Daniel Vejsada <dan.vejsada@gmail.com>
2023-11-04 10:34:07 +0100

    Add current temperature preset for heating.

--- Bathroom/scripts/update_heating_info.js
+++ Bathroom/scripts/update_heating_info.js
@@ -1,17 +1,28 @@
 function updateHeatingInfo() {
         
-    // Set current temp value
+    // Set current temp value on the dashboard
     var span_internal_temp = document.getElementById("internal_temp_value");
     span_internal_temp.innerText = String(Math.round(internal_temp));
     
-    // Set outside rain probability value
+    // Set internal humidity value on the dashboard
     var span_internal_humidity = document.getElementById("internal_humidity_value");
     span_internal_humidity.innerText = String(Math.round(internal_humidity));
 
-    // Set heating info opacity
+    // Set heating icon opacity based on the
     var opacity = bathroom_heating/100
     if (opacity < 0.2) {
+        // Minimum opacity so the icon is visible even if no heating
         opacity = 0.2
     }
     document.getElementById("flame").style.opacity = opacity;
+
+    // Set the current temperature preset to dashboard
+
+    if (String(heating_target_temp).length < 3) {
+        document.getElementById("temp_text").innerText = heating_target_temp + ".0";
+    }
+    else {
+        document.getElementById("temp_text").innerText = heating_target_temp;
+    }
+
 }
