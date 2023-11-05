d09b35d3af062574bc0f75b1acd1ce71383a40d2
Daniel Vejsada <dan.vejsada@gmail.com>
2023-11-05 15:52:30 +0100

    Implementing click event for auto button.

--- Bathroom/scripts/event_listeners.js
+++ Bathroom/scripts/event_listeners.js
@@ -1,3 +1,5 @@
+// Set event listeners for buttons
+
 document.getElementById("e2_img").addEventListener("click", (evt) =>  play_radio("media-source://radio_browser/229a2972-2d42-4f57-94a9-da84211bd575"));
 document.getElementById("fajn_img").addEventListener("click", (evt) =>  play_radio("media-source://radio_browser/8c5750b4-6870-11ea-9d09-52543be04c81"));
 document.getElementById("city_img").addEventListener("click", (evt) =>  play_radio("media-source://radio_browser/cc25c606-91ab-436f-a4ea-c3b151eb1e10"));
@@ -5,6 +7,9 @@ document.getElementById("vol_plus").addEventListener("click", (evt) => vol_chang
 document.getElementById("vol_minus").addEventListener("click", (evt) => vol_change("down"));
 document.getElementById("temp_plus").addEventListener("click", (evt) => temp_change("up"));
 document.getElementById("temp_minus").addEventListener("click", (evt) => temp_change("down"));
+document.getElementById("cal_img_wrapper").addEventListener("click", (evt) => heat_mode_change("auto"));
+
+// Set functions for buttons
 
 function play_radio(source) {
     callService("media_player.obyvak_speaker", "play_media", "media_player", {media_content_id: source, media_content_type: "audio/mpeg"});
@@ -31,4 +36,8 @@ function temp_change(change) {
         new_temp = heating_target_temp - 0.5
     }
     callService(entity_heating_target_temp[1], "set_temperature", "climate", {temperature: new_temp});
+}
+
+function heat_mode_change(mode) {
+    callService(entity_heating_target_temp[1], "set_hvac_mode", "climate", {hvac_mode: mode});
 }
